c2146c93a29ac3dc428af09c84db83c7ae1147ad
mosesochacha <ochachamoses443@gmail.com>
2022-11-29 08:21:51 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    return bob.toUpperCase;
+}
+function upperCaseCustomerName(){
+    return window.customerName = "BOB";
+    upperCaseCustomerName();
+    return window.customerName = "BOB"
+}
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "test"
+}
